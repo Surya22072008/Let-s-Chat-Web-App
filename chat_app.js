ea6374357953b3c2c5fe7fd1cb87c05d1f786c89
Surya22072008 <78208952+Surya22072008@users.noreply.github.com>
2021-06-17 15:04:21 +0530

    Add files via upload

---
+++ chat_app.js
@@ -0,0 +1,10 @@
+
+function addUser() {
+
+  user_name = document.getElementById("user_name").value;
+
+  localStorage.setItem("user_name", user_name);
+  
+    window.location = "let's_chat_web_app.html";
+}
+
--- chat_room.js
+++ chat_room.js
@@ -1,20 +1,35 @@
 
 //ADD YOUR FIREBASE LINKS HERE
 var firebaseConfig = {
-      apiKey: "AIzaSyBAuhtjEsZ-KB5uvtxfZ7PHt4YWCoGnQ8w",
-      authDomain: "chatapp-81c86.firebaseapp.com",
-      projectId: "chatapp-81c86",
-      storageBucket: "chatapp-81c86.appspot.com",
-      messagingSenderId: "703933445933",
-      appId: "1:703933445933:web:e307a1dbcef12e3388c5a4"
+      apiKey: "AIzaSyA9HIgjBP2eG-4Vz5_ziOwHS7NN0OMYLAE",
+      authDomain: "let-s-chat-web-app-78b84.firebaseapp.com",
+      databaseURL: "https://let-s-chat-web-app-78b84-default-rtdb.firebaseio.com",
+      projectId: "let-s-chat-web-app-78b84",
+      storageBucket: "let-s-chat-web-app-78b84.appspot.com",
+      messagingSenderId: "25342280750",
+      appId: "1:25342280750:web:0f01cf4dd292e78fdf4bbf"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
+    user_name = localStorage.getItem("user_name");
+    function addRoom() {
+room_name = document.getElementById("room_name").value;
+firebase.database().ref("/").child(room_name).update({purpose : "Adding room name"});
+localStorage.setItem("room_name",room_name);
+window.location = "let's_chat_web_app.html";}
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
 
+console.log("Room name - "+ Room_names);
+row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)>#"+ Room_names+"</div><hr>";
+document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+function redirectToRoomName(name) {
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location = "let's_chat_web_app.html";
+}
